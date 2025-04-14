1053e0633a0d4c05995e65d1e86c0ca394b25157
caioireno <caio_ireno@hotmail.com>
2025-04-14 17:32:57 -0300

    feat: card component to get

--- src/app/alunos/page.tsx
+++ src/app/alunos/page.tsx
@@ -1,18 +1,29 @@
+import Card from "@/components/Card";
 import { AlunoProps, getAlunos } from "@/service/ServiceAlunos";
 
 export default async function Alunos() {
   const alunos = (await getAlunos()) as AlunoProps[];
 
   return (
-    <div>
-      <h1>Lista de Alunos</h1>
-      <ul>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-semibold mb-4">Lista de Alunos</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {alunos.map((aluno) => (
-          <li key={aluno.id}>
-            <a href={`/alunos/${aluno.id}`}>{aluno.nome}</a>
-          </li>
+          <Card
+            key={aluno.id}
+            title={aluno.nome}
+            content={
+              <>
+                <p>Idade: {aluno.idade}</p>
+                <p>Turma ID: {aluno.turma_id}</p>
+                <p>Nota do 1º Semestre: {aluno.nota_primeiro_semestre}</p>
+                <p>Nota do 2º Semestre: {aluno.nota_segundo_semestre}</p>
+                <p>Média Final: {aluno.media_final}</p>
+              </>
+            }
+          />
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+// components/Card.tsx
+interface CardProps {
+  title: string;
+  content: React.ReactNode;
+  actions?: React.ReactNode;
+}
 
-export default function Card() {
+const Card = ({ title, content, actions }: CardProps) => {
   return (
-    <div>
-      <h1>meu componente</h1>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4">
+      <div className="text-center">
+        <h2 className="text-xl font-bold mb-2">{title}</h2>
+        <div className="text-gray-600">{content}</div>
+        {actions && <div className="mt-4">{actions}</div>}
+      </div>
     </div>
   );
-}
+};
+
+export default Card;
